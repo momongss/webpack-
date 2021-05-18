2ae2d5bd9bd724138ae73817f202bd08aba760fd
TC <37568161+momongss@users.noreply.github.com>
2021-05-18 14:33:18 +0900

    tmp

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./components/Hello.jsx";
+// index.html 에 작성한 ID 값
+ReactDOM.render(<Hello />, document.getElementById("root"));
